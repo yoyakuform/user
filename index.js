7687b9246ddca39a0dc05e3d20f24b26c2ce03f2
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-27 15:47:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,13 @@
-$(function () {
-    // カレンダー
-    $(function() {
-  $("#datepicker").datepicker({
-    dateFormat: 'yy-mm-dd D',
-    showAnim: 'show',
-    changeYear: true,  // 年選択プルダウン
-    changeMonth: true  // 月選択プルダウン
-    yearRange: "1922:2022"  // 年の表示範囲
+$(function(){
+  //calenderクラスの要素にカレンダーを表示し、選択できるようにする
+  $(".calender").datepicker({
+    firstDay: 1,
+    dayNames: ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
+    dayNamesMin: ["日","月","火","水","木","金","土"],
+    dayNamesShort: ["日","月","火","水","木","金","土"],
+    monthNames: ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],
+    dateFormat: "yy年mm月dd日",
+    showAnim: "slideDown",
+    changeYear: "true"
   });
-    
-    // カレンダー
-    $(function () {
-        $('input[name="date02"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-    
-    // カレンダー
-    $(function () {
-        $('input[name="date03"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-    
-    // 参加人数分の氏名欄を生成
-    $('#form-number').click(function () {
-        $('#form-name').empty();
-        var num = $('input[name="number"]:checked').val();
-        for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-            );
-        }
-    });
-
-    // 送信
-    $('form').submit(function () {
-        var date01 = $('input[name="date01"]').val();
-        var time01 = $('input[name="time01"]').val();
-        var date02 = $('input[name="date02"]').val();
-        var time02 = $('input[name="time02"]').val();
-        var date03 = $('input[name="date03"]').val();
-        var time03 = $('input[name="time03"]').val();
-        var number = $('input[name="number"]:checked').val();
-        var names = '';
-        $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
-        })
-        names = names.slice(0, -1);
-        var tel = $('input[name="tel"]').val();
-        var text = $('input[name="text"]').val();
-
-        var msg = `●第一希望日：${date01}\n∟希望時間：${time01}\n●第二希望日：${date02}\n∟希望時間：${time02}\n●第三希望日：${date03}\n∟希望時間：${time03}\n●人数：${number}\n●氏名：${names}\n●電話番号：${tel}\n●備考：${text}`;
-        sendText(msg);
-
-        return false;
-    });
 });
