1f6819836f04d005cff9a37aa5a19d62d785d684
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-27 17:29:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,31 +9,6 @@ $(function () {
         });
     });
     
-    // カレンダー
-    $(function () {
-        $('input[name="date02"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-    
-    // カレンダー
-    $(function () {
-        $('input[name="date03"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-    
-    // 参加人数分の氏名欄を生成
-    $('#form-number').click(function () {
-        $('#form-name').empty();
-        var num = $('input[name="number"]:checked').val();
-        for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-            );
-        }
-    });
-
     // 送信
     $('form').submit(function () {
         var name = $('input[name="name"]').val();
